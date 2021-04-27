c4572b82cdf4e91f01f01f6199b3f4faaec45bca
Paige Vannelli <paigevannelli@gmail.com>
2021-04-27 10:25:44 -0700

    Delete commneted out code

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 class OrderForm extends Component {
   constructor(props) {
     super(props);
-    // this.props = props;
     this.state = {
       name: '',
       ingredients: []
